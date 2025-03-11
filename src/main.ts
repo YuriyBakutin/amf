8e8df58f98e635723ea403714fe43f345a14b598
Yuriy Bakutin <yuriybakutin@gmail.com>
2025-03-11 08:02:55 +0300

    Добавлена библиотека компонентов Vant

--- src/main.ts
+++ src/main.ts
@@ -3,8 +3,10 @@ import '@/style.css'
 import App from '@/App.vue'
 import { createPinia } from 'pinia'
 import piniaPluginPersistedstate from 'pinia-plugin-persistedstate'
+import Vant from 'vant'
+import 'vant/lib/index.css'
 
 const pinia = createPinia()
 pinia.use(piniaPluginPersistedstate)
 
-createApp(App).use(pinia).mount('#app')
+createApp(App).use(Vant).use(pinia).mount('#app')
